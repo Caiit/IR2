d1f00b6cba250d1d95911ac2ac57907515e36085
Houda <houda1996@hotmail.com>
2018-12-18 11:25:14 +0100

    gpu support for saliency

--- code/rerankwrite/train_saliency.py
+++ code/rerankwrite/train_saliency.py
@@ -11,6 +11,7 @@ from rouge import Rouge
 import matplotlib.pyplot as plt
 import argparse
 import numpy as np
+from tqdm import tqdm
 
 def convert_to_words(complete_sent_emb, w2emb):
     output_sentence = []
@@ -24,7 +25,7 @@ def convert_to_words(complete_sent_emb, w2emb):
 def train(args):
     print("Load data...")
     data_train = load_data(args.folder + "/train_data.json")
-    data_test = load_data(args.folder + "/test_data.json")
+    data_test = load_data(args.folder + "/dev_data.json")
     embeddings = load_pickle(args.embeddings)
     w2i = load_pickle(args.w2i)
     w2emb = load_pickle(args.w2emb)
@@ -37,18 +38,21 @@ def train(args):
                                             "constant", constant_values=(len(w2i))) for temp1 in cut_templates]
     actual_templates = [convert_to_words(sent, w2emb) for sent in flattened_templates_emb]
 
+    print("Now load the model...")
     emb_size = len(embeddings[0])
     model = SaliencyPrediction(emb_size*args.max_length)
+    if args.use_gpu:
+        model.cuda()
     #loss_func = nn.NLLLoss()
     loss_func = nn.BCELoss()
-    optimizer = optim.Adam(model.parameters())
+    #optimizer = optim.Adam(model.parameters())
+    optimizer = optim.SGD(model.parameters(), lr = 0.001, momentum=0.9)
     rouge = Rouge()
 
-    for example in data_train[:100]:
-        resources = []
-        embedded_resources = []
-        class_indices = []
-
+    print("Read in train data...")
+    resources = []
+    embedded_resources = []
+    for example in tqdm(data_train):
         get_resources(example["documents"]["comments"], resources,
                       embedded_resources, embeddings, w2i)
         num_comments = len(resources)
@@ -62,11 +66,30 @@ def train(args):
                       embedded_resources, embeddings, w2i)
         num_reviews = len(resources) - num_comments - num_facts - num_plots
 
-    # Not yet batch
+
+    print("Read in test data...")
+    resources_test = []
+    embedded_resources_test = []
+    for example in tqdm(data_test):
+        get_resources(example["documents"]["comments"], resources_test,
+                      embedded_resources_test, embeddings, w2i)
+        num_comments = len(resources)
+        get_resources(example["documents"]["fact_table"], resources_test,
+                      embedded_resources_test, embeddings, w2i)
+        num_facts = len(resources) - num_comments
+        get_resources(example["documents"]["plot"], resources_test,
+                      embedded_resources_test, embeddings, w2i)
+        num_plots = len(resources) - num_comments - num_facts
+        get_resources(example["documents"]["review"], resources_test,
+                      embedded_resources_test, embeddings, w2i)
+        num_reviews = len(resources) - num_comments - num_facts - num_plots
+
+    print("Now learn.....")
     total_resources = len(embedded_resources)
-    for epoch in range(5):
+    for epoch in range(10):
         print("Epoch: " + str(epoch))
-        for i, resource in enumerate(embedded_resources):
+        avg_loss = 0
+        for i, resource in tqdm(enumerate(embedded_resources)):
             sent = " ".join(resources[i])
             if sent == "" or sent == "eod":
                 continue;
@@ -82,15 +105,60 @@ def train(args):
             all_res = torch.Tensor(padd_resource).unsqueeze(0).repeat(20, 1, 1)
             size_inp = all_res.size()
             for j, template in enumerate(flattened_templates_emb_padded):
-                actual_score = rouge.get_scores(actual_templates[j], " ".join(resources[i]))[0]["rouge-1"]["f"]
+                try:
+                    actual_score = rouge.get_scores(actual_templates[j], " ".join(resources[i]))[0]["rouge-1"]["f"]
+                except:
+                    actual_score = 0
                 actual_scores.append(actual_score)
-            scores = model.forward(all_res.reshape(size_inp[0], size_inp[1]*size_inp[2]), all_temps.reshape(size_inp[0], size_inp[1]*size_inp[2]))
-            loss = loss_func(scores, torch.Tensor(actual_scores))
+            x1 = all_res.reshape(size_inp[0], size_inp[1]*size_inp[2])
+            x2 = all_temps.reshape(size_inp[0], size_inp[1]*size_inp[2])
+            actual_scores = torch.Tensor(actual_scores).unsqueeze(1)
+            if args.use_gpu:
+                x1.cuda()
+                x2.cuda()
+                actual_scores.cuda()
+            scores = model.forward(x1, x2)
+            loss = loss_func(scores, actual_scores)
+            avg_loss += loss.item()
             loss.backward()
             optimizer.step()
-            print("Step: " + str(i) + "/" + str(total_resources) + ", the loss is: " + str(loss.item()))
+        #print("Step: " + str(i) + "/" + str(total_resources) + ", the loss is: " + str(loss.item()))
+        print("For this epoch, we found avg_loss: " + str(avg_loss/total_resources))
 
+
+        total_loss = 0
+        amount_res = len(resources)
+        for i, resource in tqdm(enumerate(embedded_resources_test)):
+            sent = " ".join(resources_test[i])
+            if sent == "" or sent == "eod":
+                continue;
+            padd_resource = resource[-args.max_length:]
+            padd_resource = np.pad(padd_resource, ((0, args.max_length - len(padd_resource)), (0, 0)), "constant",
+                                constant_values=(len(w2i)))
+            tensor_templates = []
+            actual_scores = []
+            all_temps = torch.Tensor(flattened_templates_emb_padded)
+            all_res = torch.Tensor(padd_resource).unsqueeze(0).repeat(20, 1, 1)
+            size_inp = all_res.size()
+            for j, template in enumerate(flattened_templates_emb_padded):
+                try:
+                    actual_score = rouge.get_scores(actual_templates[j], " ".join(resources[i]))[0]["rouge-1"]["f"]
+                except:
+                    actual_score = 0
+                actual_scores.append(actual_score)
+            x1 = all_res.reshape(size_inp[0], size_inp[1]*size_inp[2])
+            x2 = all_temps.reshape(size_inp[0], size_inp[1]*size_inp[2])
+            actual_scores = torch.Tensor(actual_scores).unsqueeze(1)
+            if args.use_gpu:
+                x1.cuda()
+                x2.cuda()
+                actual_scores.cuda()
+            scores = model.forward(x1, x2)
+            loss = loss_func(scores, actual_scores)
+            total_loss += loss.item()
+        print("Average loss is: " + str(total_loss/amount_res))
     torch.save(model, "../../models/rewrite/saliency.pt")
+
     return
 
 
@@ -104,6 +172,7 @@ if __name__ == "__main__":
     parser.add_argument("--w2i", help="path to file of the saved w2i", default="../../embeddings/w2i.pkl")
     parser.add_argument("--w2emb", help="path to the file of the saved w2emb", default="../../embeddings/w2emb.pkl")
     parser.add_argument("--max_length", help="max length of sentences", default=110)
+    parser.add_argument("--use_gpu", help="whether to use gpu or not", type=bool, default=False)
 
     args = parser.parse_args()
     train(args)
