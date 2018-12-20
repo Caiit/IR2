fe833f76bcee7d0e220d5423a144b49fc53df7a8
Tirza <tirza.soute@student.uva.nl>
2018-12-21 00:01:39 +0100

    try new saliency

--- code/rerankwrite/train_saliency.py
+++ code/rerankwrite/train_saliency.py
@@ -12,9 +12,9 @@ import matplotlib.pyplot as plt
 import argparse
 import numpy as np
 from tqdm import tqdm
+import os
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = "cpu"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def convert_to_words(complete_sent_emb, w2emb):
     output_sentence = []
@@ -25,31 +25,52 @@ def convert_to_words(complete_sent_emb, w2emb):
 
     return " ".join(output_sentence)
 
+
+def make_templates(args, templates_emb, w2emb, w2i):
+    """
+    Gets embedded + unembedded templates if they don't exist.
+    """
+
+    if os.path.exists("templates.pkl"):
+        templates = load_pickle("templates.pkl")
+        templates_emb_pad = load_pickle("templates_emb.pkl")
+    else:
+        # Flatten templates
+        templates_emb = [y for x in templates_emb for y in x]
+        # Cut templates to maximum length
+        templates_emb = [temp[-args.max_length:] for temp in templates_emb]
+        # Pad embeddings
+        templates_emb_pad = [np.pad(temp1, ((0, args.max_length - len(temp1)),
+                             (0, 0)), "constant", constant_values=(len(w2i)))
+                             for temp1 in templates_emb]
+        # Convert embedded templates to word templates
+        templates = [convert_to_words(sent, w2emb) for sent in templates_emb]
+
+        save_pickle("templates.pkl", templates)
+        save_pickle("templates_emb.pkl", templates_emb_pad)
+    return templates, templates_emb_pad
+
 def train(args):
     print("Load data...")
-    data_train = load_data(args.folder + "/train_data.json")
-    data_test = load_data(args.folder + "/dev_data.json")
+    data_train = load_data(args.folder + "/train_data.json")[:5]
+    data_test = load_data(args.folder + "/dev_data.json")[:5]
     embeddings = load_pickle(args.embeddings)
     w2i = load_pickle(args.w2i)
     w2emb = load_pickle(args.w2emb)
     templates_emb  = get_templates("../../data/templates.pkl")
 
+
     print("Do the templates...")
-    flattened_templates_emb = [y for x in templates_emb for y in x]
-    cut_templates = [temp[-args.max_length:] for temp in flattened_templates_emb]
-    flattened_templates_emb_padded = [np.pad(temp1, ((0, args.max_length - len(temp1)), (0, 0)),
-                                            "constant", constant_values=(len(w2i))) for temp1 in cut_templates]
-    actual_templates = [convert_to_words(sent, w2emb) for sent in flattened_templates_emb]
+    templates, templates_emb = make_templates(args, templates_emb, w2emb, w2i)
+
 
     print("Now load the model...")
     emb_size = len(embeddings[0])
-    model = SaliencyPrediction(emb_size*args.max_length)
-    model.to(device)
-    #loss_func = nn.NLLLoss()
+    model = SaliencyPrediction(emb_size*args.max_length, device).to(device)
     loss_func = nn.BCELoss()
-    #optimizer = optim.Adam(model.parameters())
     optimizer = optim.SGD(model.parameters(), lr = 0.001, momentum=0.9)
     rouge = Rouge()
+
 
     print("Read in train data...")
     resources = []
@@ -88,6 +109,8 @@ def train(args):
 
     print("Now learn.....")
     total_resources = len(embedded_resources)
+    all_temps = torch.Tensor(templates_emb)
+
     for epoch in range(5):
         print("Epoch: " + str(epoch))
         avg_loss = 0
@@ -96,36 +119,31 @@ def train(args):
             if sent == "" or sent == "eod":
                 continue;
             optimizer.zero_grad()
-            #all_scores = []
-            #all_real_scores = []
+
             padd_resource = resource[-args.max_length:]
-            padd_resource = np.pad(padd_resource, ((0, args.max_length - len(padd_resource)), (0, 0)), "constant",
-                                constant_values=(len(w2i)))
-            tensor_templates = []
+            padd_resource = np.pad(padd_resource, ((0, args.max_length -
+                                   len(padd_resource)), (0, 0)), "constant",
+                                   constant_values=(len(w2i)))
+
             actual_scores = []
-            all_temps = torch.Tensor(flattened_templates_emb_padded)
             all_res = torch.Tensor(padd_resource).unsqueeze(0).repeat(20, 1, 1)
             size_inp = all_res.size()
-            for j, template in enumerate(flattened_templates_emb_padded):
+            for j, template in enumerate(templates_emb):
                 try:
-                    actual_score = rouge.get_scores(actual_templates[j], " ".join(resources[i]))[0]["rouge-1"]["f"]
+                    actual_score = rouge.get_scores(templates[j], " ".join(resources[i]))[0]["rouge-1"]["f"]
                 except:
                     actual_score = 0
                 actual_scores.append(actual_score)
-            x1 = all_res.reshape(size_inp[0], size_inp[1]*size_inp[2])
-            x2 = all_temps.reshape(size_inp[0], size_inp[1]*size_inp[2])
-            actual_scores = torch.Tensor(actual_scores).unsqueeze(1)
-            x1.to(device)
-            x2.to(device)
-            actual_scores.to(device)
-            scores = model.forward(x1, x2)
+            x1 = all_res.reshape(size_inp[0], size_inp[1]*size_inp[2]).to(device)
+            x2 = all_temps.reshape(size_inp[0], size_inp[1]*size_inp[2]).to(device)
+            actual_scores = torch.Tensor(actual_scores).unsqueeze(1).to(device)
+            scores = model(x1, x2)
             loss = loss_func(scores, actual_scores)
             avg_loss += loss.item()
             loss.backward()
             optimizer.step()
-        #print("Step: " + str(i) + "/" + str(total_resources) + ", the loss is: " + str(loss.item()))
         print("For this epoch, we found avg_loss: " + str(avg_loss/total_resources))
-        torch.save(model, "../../models/rewrite/saliency.pt")
+        torch.save(model, "../../models/rewrite/saliency_" + str(epoch) + ".pt")
 
 
         total_loss = 0
@@ -135,36 +153,26 @@ def train(args):
             if sent == "" or sent == "eod":
                 continue;
             padd_resource = resource[-args.max_length:]
-            padd_resource = np.pad(padd_resource, ((0, args.max_length - len(padd_resource)), (0, 0)), "constant",
-                                constant_values=(len(w2i)))
-            tensor_templates = []
+            padd_resource = np.pad(padd_resource, ((0, args.max_length -
+                                   len(padd_resource)), (0, 0)), "constant",
+                                   constant_values=(len(w2i)))
             actual_scores = []
-            all_temps = torch.Tensor(flattened_templates_emb_padded)
             all_res = torch.Tensor(padd_resource).unsqueeze(0).repeat(20, 1, 1)
             size_inp = all_res.size()
-            for j, template in enumerate(flattened_templates_emb_padded):
+            for j, template in enumerate(templates_emb):
                 try:
-                    actual_score = rouge.get_scores(actual_templates[j], " ".join(resources[i]))[0]["rouge-1"]["f"]
+                    actual_score = rouge.get_scores(templates[j], " ".join(resources[i]))[0]["rouge-1"]["f"]
                 except:
                     actual_score = 0
                 actual_scores.append(actual_score)
-            x1 = all_res.reshape(size_inp[0], size_inp[1]*size_inp[2])
-            x2 = all_temps.reshape(size_inp[0], size_inp[1]*size_inp[2])
-            actual_scores = torch.Tensor(actual_scores).unsqueeze(1)
+            x1 = all_res.reshape(size_inp[0], size_inp[1]*size_inp[2]).to(device)
+            x2 = all_temps.reshape(size_inp[0], size_inp[1]*size_inp[2]).to(device)
+            actual_scores = torch.Tensor(actual_scores).unsqueeze(1).to(device)
 
-            if i % 100 == 0:
-                print("Iteration", str(i))
-
-            x1.to(device)
-            x2.to(device)
-            actual_scores.to(device)
-            scores = model.forward(x1, x2)
+            scores = model(x1, x2)
             loss = loss_func(scores, actual_scores)
             total_loss += loss.item()
         print("Average loss is: " + str(total_loss/amount_res))
-
-    return
-
 
 
 
