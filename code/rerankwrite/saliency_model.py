7068a6fb48438aec844469d35710b2c6189d2559
Tirza <tirza.soute@student.uva.nl>
2018-12-21 00:02:27 +0100

    try extra device

--- code/rerankwrite/saliency_model.py
+++ code/rerankwrite/saliency_model.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 
 class SaliencyPrediction(nn.Module):
-    def __init__(self, embedding_size):
+    def __init__(self, embedding_size, device):
         super(SaliencyPrediction, self).__init__()
         # Maybe extend this further?
-        self.bilinear = nn.Bilinear(embedding_size, embedding_size, 1)
+        self.bilinear = nn.Bilinear(embedding_size, embedding_size, 1).to(device)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, resource_embed, template_embed):
